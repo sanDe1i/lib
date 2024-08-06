33d507c9205b9f20cdc7e83f8ac8c17d7fb33a0d
crx <912396824@qq.com>
2024-08-06 16:50:05 +0800

    增加epublist

--- demo/src/main/java/com/example/lm/Controller/ListBookController.java
+++ demo/src/main/java/com/example/lm/Controller/ListBookController.java
@@ -7,18 +7,13 @@ import com.example.lm.Service.FileService;
 import com.example.lm.Service.ResourcesLibService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.net.URLEncoder;
 import java.nio.file.Path;
 import java.nio.file.Paths;
